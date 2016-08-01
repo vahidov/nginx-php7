0c0fa090d5e362730fc33469fcac27417d9744aa
skiy <dev@skiy.net>
2016-08-02 03:51:34 +0800

    update dockerfile

--- start.sh
+++ start.sh
@@ -26,8 +26,7 @@ if [[ -n "$PROXY_WEB" ]]; then
     # FREE HTTPS
     if [[ -n "$PROXY_FREE" ]]; then
 
-        yum -y install epel-release python
-        yum install -y openssl
+        yum -y install python python-pip openssl
         #Let's FREE HTTPS
         curl -O -SL -C - https://github.com/certbot/certbot/archive/v0.8.1.tar.gz  && \
         tar -zxvf v0.8.1.tar.gz && \
